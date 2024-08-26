136176e4f90d3b36cdff7925926976d714084e6f
jerpuen <kittipot.kulpinit@gmail.com>
2024-08-26 08:43:33 +0700

    sent: assignment

--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,24 @@
 // Start coding here
+let add = function (a,b) {
+    return a + b;
+};
+let resultAdd = add(10,5);
+console.log(resultAdd);
+
+let subtract = function (a,b) {
+    return a - b;
+};
+let resultSubtract = subtract(10,5);
+console.log(resultSubtract);
+
+let multiply = function (a,b) {
+    return a * b;
+};
+let resultMultiply = multiply(10,5);
+console.log(resultMultiply);
+
+let divide = function (a,b) {
+    return a / b;
+};
+let resultDivide = divide(10,5);
+console.log(resultDivide);
--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,32 @@
 // Start coding here
 let calculator = {};
+
+let add = (a,b) => a + b;
+console.log(add(10,5));
+
+let subtract = (a,b) => a - b;
+console.log(subtract(10,5));
+
+let multiply = (a,b) => a * b;
+console.log(multiply(10,5));
+
+let divide = (a,b) => a / b;
+console.log(divide(10,5));
+
+calculator.add = add(10,5);
+calculator.subtract = subtract(10,5);
+calculator.multiply = multiply(10,5);
+calculator.divide = divide(10,5);
+
+console.log(calculator);
+
+calculator.add = add(10,20);
+let addResult = calculator.add;
+console.log(addResult);
+
+calculator.divide = divide(3000,10);
+let divideResult = calculator.divide;
+console.log(divideResult);
+
+console.log(calculator);
+
